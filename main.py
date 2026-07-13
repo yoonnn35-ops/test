c6974f5d6f695d73cae1fd84d37677d701d2dc16
yoonnn35-ops <yoonnn35@gmail.com>
2026-07-13 10:43:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ st.write("안녕")
 
 지역 = st.selectbox("지역을 골라 보세요", ["서울", "양평", "부산"])
 st.write("당신이 고른 지역:", 지역)
+
+숫자 = st.slider("좋아하는 숫자", 0, 100)
+st.write("고른 숫자:", 숫자)
+ 
+if st.button("풍선 날리기"):
+	st.balloons()
